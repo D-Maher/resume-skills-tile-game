337b5832574f7785f7f9e3eb544e60e2061e9855
Danny Maher <dmaher724@gmail.com>
2017-04-24 16:12:43 -0500

    Write Game constuctor function to initialize a new game with the board represented as a nested array, write function to convert nested array into nice-looking console output

---
+++ js/game.js
@@ -0,0 +1,26 @@
+function Game() {
+  this.board = [[0, 0, 2, 0],
+                [0, 0, 0, 0],
+                [0, 0, 2, 0],
+                [0, 4, 0, 0]];
+  this.prettyBoard = this.prettify(this.board);
+}
+
+
+Game.prototype.prettify = function(board) {
+  var prettyBoard = ""
+
+  for (var i = 0; i < board.length; i++) {
+    var rowString = "|"
+
+    for (var j = 0; j < board.length; j++) {
+      rowString += board[i][j].toString() + "|";
+    }
+
+    rowString += "\n"
+
+    prettyBoard += rowString
+  }
+
+  return prettyBoard;
+}
