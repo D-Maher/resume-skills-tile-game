78ee2f06e92c24886247da709fdabd54b75e87e3
Danny Maher <dmaher724@gmail.com>
2017-04-24 17:40:14 -0500

    Add findDestination function to determine destination of each tile on the board when moving left, add moveLeft function to move tiles to the left based on their destinations

--- js/game.js
+++ js/game.js
@@ -24,10 +24,62 @@ Game.prototype.prettify = function(board) {
   return prettyBoard;
 }
 
-function isValidMove(currentValue, destValue) {
-  if (destValue === 0 || currentValue === destValue) {
+function isValidMove(currentValue, destinationValue) {
+  if (destinationValue === 0 || currentValue === destinationValue) {
     return true;
   } else {
     return false;
+  }
+}
+
+function findDestination(currentCoords, direction) {
+  var board = game.board;
+
+  var currentRow = currentCoords[0];
+  var currentColumn = currentCoords[1];
+
+  switch(direction) {
+    case "left":
+
+      for (var lastCheckedColumn = currentColumn - 1; lastCheckedColumn >= 0; lastCheckedColumn--) {
+        var currentValue = board[currentRow][currentColumn];
+        var destinationValue = board[currentRow][lastCheckedColumn]
+
+        if (isValidMove(currentValue, destinationValue) === false) {
+          return [currentRow, lastCheckedColumn + 1];
+        }
+      }
+
+      return [currentRow, 0]
+  }
+}
+
+Game.prototype.moveLeft = function() {
+  var board = this.board;
+  var moved = false;
+
+  console.log("LEFT")
+
+  for (var row = 0; row <= 3; row++) {
+    for (var column = 1; column <= 3; column++) {
+      var currentValue = board[row][column]
+
+      if (currentValue !== 0) {
+        var destination = findDestination([row, column], "left");
+
+        var destinationRow = destination[0];
+        var destinationColumn = destination[1];
+
+        var destinationValue = board[destinationRow][destinationColumn];
+
+        if (destinationValue === currentValue) {
+          board[destinationRow][destinationColumn] = currentValue + destinationValue
+          board[row][column] = 0
+        } else {
+          board[row][column] = 0
+          board[destinationRow][destinationColumn] = currentValue
+        }
+      }
+    }
   }
 }
