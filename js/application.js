aa77ba79688e5e1662099fd8b41947797759ff5d
Danny Maher <dmaher724@gmail.com>
2017-04-26 13:33:17 -0500

    Add updateBoard function to update the board on the screen based on the board array within the Game model

--- js/application.js
+++ js/application.js
@@ -4,29 +4,51 @@ $(document).ready(function() {
 
   game.spawnTile();
   game.spawnTile();
-  game.printBoard();
+  updateBoard();
 
   Mousetrap.bind("left", function() {
     game.moveLeft();
     game.spawnTile();
-    game.printBoard();
+    updateBoard();
   })
 
   Mousetrap.bind("right", function() {
     game.moveRight();
     game.spawnTile();
-    game.printBoard();
+    updateBoard();
   })
 
   Mousetrap.bind("up", function() {
     game.moveUp();
     game.spawnTile();
-    game.printBoard();
+    updateBoard();
   })
 
   Mousetrap.bind("down", function() {
     game.moveDown();
     game.spawnTile();
-    game.printBoard();
+    updateBoard();
   })
 });
+
+function updateBoard() {
+  var flattenedBoard = [];
+
+  for (var i = 0; i < game.board.length; i++) {
+    for (var j = 0; j < game.board[i].length; j++) {
+      flattenedBoard.push(game.board[i][j])
+    }
+  }
+
+  for (var k = 0; k < $('td').length; k++) {
+    var $tile = $('td').eq(k);
+
+    $tile.html(flattenedBoard[k])
+
+    if ($tile.html() === "0") {
+      $tile.html("")
+    }
+  }
+
+  game.printBoard();
+};
